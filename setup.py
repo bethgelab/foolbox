e979dfdf43df53f56e56035446e3f1138c771341
Jonas Rauber <git@jonasrauber.de>
2017-06-04 21:18:49 +0000

    created package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='foolbox',
+      version='0.1a1',
+      url='https://github.com/jonasrauber/foolbox',
+      author='Jonas Rauber & Wieland Brendel',
+      author_email='opensource@bethgelab.org',
+      packages=find_packages(),
+      zip_safe=False)
