5dbc6f89e08df096033f02c27b985d13b9ff8d0e
Jonas Rauber <git@jonasrauber.de>
2017-06-06 13:44:04 +0000

    added distances

---
+++ foolbox/distances.py
@@ -0,0 +1,118 @@
+"""
+Provides classes to measure the distance between images.
+
+Distances
+---------
+
+.. autosummary::
+   :nosignatures:
+
+   MeanSquaredDistance
+   MeanAbsoluteDistance
+
+Aliases
+-------
+
+.. autosummary::
+   :nosignatures:
+
+   MSE
+
+Base class
+----------
+
+To implement a new distance, simply subclass the :class:`Distance` class and
+implement the :meth:`_calculate` method.
+
+.. autosummary::
+   :nosignatures:
+
+   Distance
+
+Examples
+--------
+
+>>> from foolbox.distances import MeanSquaredDistance
+>>> d = MeanSquaredDistance(np.array([1, 2]), np.array([2, 2]))
+>>> print(d)
+MSE = 5.000000e-01 (rel. MSE = 20.0 %)
+>>> assert d.value() == 0.5
+
+"""
+
+from abc import ABC, abstractmethod
+import functools
+import numpy as np
+
+
+@functools.total_ordering
+class Distance(ABC):
+
+    def __init__(self, reference, other):
+        self.reference = reference
+        self.other = other
+        # gradient of value w.r.t. other
+        self._value, self._gradient = self._calculate()
+
+    def value(self):
+        return self._value
+
+    def gradient(self):
+        return self._gradient
+
+    @abstractmethod
+    def _calculate(self):
+        raise NotImplementedError
+
+    def name(self):
+        return self.__class__.__name__
+
+    def __str__(self):
+        return '{} = {:.6e}'.format(self.name(), self._value)
+
+    def __eq__(self, other):
+        if other.__class__ != self.__class__:
+            raise NotImplementedError('Comparisons are only possible between the same distance types.')  # noqa: E501
+        return self._value.__eq__(other)
+
+    def __lt__(self, other):
+        if other.__class__ != self.__class__:
+            raise NotImplementedError('Comparisons are only possible between the same distance types.')  # noqa: E501
+        return self.value().__lt__(other.value())
+
+
+class MeanSquaredDistance(Distance):
+
+    def _calculate(self):
+        diff = self.other - self.reference
+        value = np.mean(np.square(diff))
+        n = np.prod(self.reference.shape)
+        gradient = 1 / n * 2 * diff
+
+        rel = value / np.mean(np.square(self.reference)) * 100
+        self._rel = rel
+        return value, gradient
+
+    def __str__(self):
+        return 'MSE = {:.6e} (rel. MSE = {:.1f} %)'.format(
+            self._value, self._rel)
+
+
+MSE = MeanSquaredDistance
+
+
+class MeanAbsoluteDistance(Distance):
+
+    def _calculate(self):
+        diff = self.other - self.reference
+        value = np.mean(np.abs(diff))
+        n = np.prod(self.reference.shape)
+        gradient = 1 / n * np.sign(diff) * np.abs(diff)
+
+        rel = value / np.mean(np.abs(self.reference)) * 100
+        self._rel = rel
+        return value, gradient
+
+    def __str__(self):
+        return 'MAE = {:.6e} (rel. MAE = {:.1f} %)'.format(
+            self._value, self._rel)
---
+++ foolbox/tests/test_distances.py
@@ -0,0 +1,47 @@
+import pytest
+import numpy as np
+from foolbox import distances
+
+
+def test_abstract_distance():
+    with pytest.raises(TypeError):
+        distances.Distance()
+
+
+def test_base_distance():
+
+    class TestDistance(distances.Distance):
+
+        def _calculate(self):
+            return 22, 2
+
+        def __lt__(self, other):
+            return super().__lt__(other)
+
+    distance = TestDistance(None, None)
+    assert distance.name() == 'TestDistance'
+    assert distance.value() == 22
+    assert distance.gradient() == 2
+    assert '2.2' in str(distance)
+    assert 'TestDistance' in str(distance)
+    assert distance == distance
+    assert not distance < distance
+    assert not distance > distance
+    assert distance <= distance
+    assert distance >= distance
+
+
+def test_mse():
+    assert distances.MSE == distances.MeanSquaredDistance
+
+
+def test_mean_squared_distance():
+    d = distances.MeanSquaredDistance(np.array([0, 2]), np.array([2, 2]))
+    assert d.value() == 2.
+    assert (d.gradient() == np.array([2, 0])).all()
+
+
+def test_mean_absolute_distance():
+    d = distances.MeanAbsoluteDistance(np.array([0, 2]), np.array([2, 2]))
+    assert d.value() == 1.
+    assert (d.gradient() == np.array([1, 0])).all()
