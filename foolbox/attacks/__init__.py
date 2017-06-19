073a4608ff1fb2e720a7e372e8771032c1da056b
Jonas Rauber <git@jonasrauber.de>
2017-06-19 20:47:51 +0000

    using warnings instead of logging and removed DeepFool alias

--- foolbox/attacks/__init__.py
+++ foolbox/attacks/__init__.py
@@ -4,7 +4,7 @@ from .base import Attack
 from .gradientsign import FGSM, GradientSignAttack, IterativeGradientSignAttack
 from .gradient import GradientAttack, IterativeGradientAttack
 from .lbfgs import LBFGSAttack, ApproximateLBFGSAttack
-from .deepfool import DeepFool, DeepFoolAttack
+from .deepfool import DeepFoolAttack
 from .saliency import SaliencyMapAttack
 from .blur import GaussianBlurAttack
 from .contrast import ContrastReductionAttack
