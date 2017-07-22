6ac2a368b7ce23d3598cc9177bcef9ec0e6c6e53
Wieland Brendel <wieland.brendel@posteo.de>
2017-07-22 18:51:29 +0200

    fixed flake8

--- foolbox/tests/test_models_keras.py
+++ foolbox/tests/test_models_keras.py
@@ -13,7 +13,7 @@ from foolbox.models import KerasModel
 
 @pytest.mark.parametrize('num_classes', [10, 1000])
 def test_keras_model(num_classes):
-    
+
     bounds = (0, 255)
     channels = num_classes
 
@@ -50,7 +50,6 @@ def test_keras_model(num_classes):
 
 @pytest.mark.parametrize('num_classes', [10, 1000])
 def test_keras_model_probs(num_classes):
-
     bounds = (0, 255)
     channels = num_classes
 
