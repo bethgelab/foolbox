365cba61f7c507de31199befe04d10c74237cdaa
Wieland Brendel <wieland.brendel@posteo.de>
2017-08-03 18:06:32 +0200

    added default subsampling to DeepFool

--- foolbox/attacks/deepfool.py
+++ foolbox/attacks/deepfool.py
@@ -19,7 +19,7 @@ class DeepFoolAttack(Attack):
            https://arxiv.org/abs/1511.04599
     """
 
-    def _apply(self, a, steps=100, subsample=False):
+    def _apply(self, a, steps=100, subsample=20):
         if not a.has_gradient():
             return
 
