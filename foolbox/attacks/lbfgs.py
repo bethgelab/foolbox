9d4849473c7b0d6ccb8115ac9811385b57224885
Wieland Brendel <wieland.brendel@posteo.de>
2017-08-14 14:30:25 +0200

    fixed LBFGS

--- foolbox/attacks/lbfgs.py
+++ foolbox/attacks/lbfgs.py
@@ -179,7 +179,7 @@ class LBFGSAttack(Attack):
             # LBFGS-B does not always exactly respect the boundaries
             if np.amax(x) > max_ or np.amin(x) < min_:   # pragma: no coverage
                 logging.info('Image out of bounds (max, min = {}, {}). Performing manual clip.'.format(np.amin(x), np.amax(x)))  # noqa: E501
-                x = np.clip(x, max_, min_)
+                x = np.clip(x, min_, max_)
             _, is_adversarial = a.predictions(x.reshape(shape).astype(dtype))
             return is_adversarial
 
