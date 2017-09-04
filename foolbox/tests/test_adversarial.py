c2a630f36c113d33ad0e27ad306d565d556bd462
Jonas Rauber <git@jonasrauber.de>
2017-09-04 21:14:49 +0000

    full backward test coverage

--- foolbox/tests/test_adversarial.py
+++ foolbox/tests/test_adversarial.py
@@ -66,7 +66,9 @@ def test_adversarial(model, criterion, image, label):
 
     gradient_pre = np.ones_like(predictions) * 0.3
     gradient = adversarial.backward(gradient_pre, image)
+    gradient2 = adversarial.backward(gradient_pre)
     assert gradient.shape == image.shape
+    assert (gradient == gradient2).all()
 
     gradient = adversarial.gradient()
     assert gradient.shape == image.shape
