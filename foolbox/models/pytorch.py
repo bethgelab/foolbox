c08bc14bae2ae77146d312a6a99169b62d78460e
Jonas Rauber <git@jonasrauber.de>
2017-10-06 10:28:56 +0200

    added a warning when a PyTorch model is in train mode

--- foolbox/models/pytorch.py
+++ foolbox/models/pytorch.py
@@ -43,6 +43,12 @@ class PyTorchModel(DifferentiableModel):
         self._model = model
         self.cuda = cuda
 
+        if model.training:
+            warnings.warn(
+                'The PyTorch model is in training mode and therefore might'
+                ' not be deterministic. Call the eval() method to set it in'
+                ' evaluation mode if this is not intended.')
+
     def batch_predictions(self, images):
         # lazy import
         import torch
