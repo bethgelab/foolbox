88034644d4417cb9bfcb12a48b08e297a9011bde
Jonas Rauber <git@jonasrauber.de>
2018-02-16 14:47:45 +0000

    more tests

--- foolbox/tests/test_attacks_gradient.py
+++ foolbox/tests/test_attacks_gradient.py
@@ -11,6 +11,14 @@ def test_attack(bn_adversarial):
     assert adv.distance.value < np.inf
 
 
+def test_attack_eps(bn_adversarial):
+    adv = bn_adversarial
+    attack = Attack()
+    attack(adv, epsilons=np.linspace(0., 1., 100)[1:])
+    assert adv.image is not None
+    assert adv.distance.value < np.inf
+
+
 def test_attack_gl(gl_bn_adversarial):
     adv = gl_bn_adversarial
     attack = Attack()
