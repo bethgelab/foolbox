3b227182f9fee6e63e8fecf3790caa2aa76fa349
Behar Veliqi <behar@veliqi.de>
2018-08-08 13:11:34 +0200

    added test_attack_gl for newtonfool

--- foolbox/tests/test_attacks_newtonfool.py
+++ foolbox/tests/test_attacks_newtonfool.py
@@ -11,6 +11,14 @@ def test_attack(bn_adversarial):
     assert adv.distance.value < np.inf
 
 
+def test_attack_gl(gl_bn_adversarial):
+    adv = gl_bn_adversarial
+    attack = Attack()
+    attack(adv)
+    assert adv.image is None
+    assert adv.distance.value == np.inf
+
+
 def test_targeted_attack(bn_targeted_adversarial):
     adv = bn_targeted_adversarial
     attack = Attack()
