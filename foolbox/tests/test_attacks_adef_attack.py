d8d527c0eb6e64eb6b7aa299b324028d64ca7ca5
Evgenia Rusak <rusak@fzi.de>
2018-08-15 18:24:15 +0200

    added a test for pytorch, should increase coverage for line 124

--- foolbox/tests/test_attacks_adef_attack.py
+++ foolbox/tests/test_attacks_adef_attack.py
@@ -3,6 +3,14 @@ import numpy as np
 from foolbox.attacks import ADefAttack as Attack
 
 
+def test_attack_pytorch(bn_adversarial_pytorch):
+    adv = bn_adversarial_pytorch
+    attack = Attack()
+    attack(adv)
+    assert adv.image is not None
+    assert adv.distance.value < np.inf
+
+
 def test_attack(bn_adversarial):
     adv = bn_adversarial
     attack = Attack()
