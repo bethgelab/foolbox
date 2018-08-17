4b146cea7c4b486d99f8c1bd9b017ae23f0d34f4
Evgenia Rusak <rusak@fzi.de>
2018-08-17 15:02:43 +0200

    fixed a test

--- foolbox/tests/test_attacks_adef_attack.py
+++ foolbox/tests/test_attacks_adef_attack.py
@@ -27,12 +27,12 @@ def test_attack(bn_adversarial):
     assert adv.distance.value < np.inf
 
 
-def test_attack_gl(gl_bn_adversarial):
+def test_attack_gl(Attack, gl_bn_adversarial):
     adv = gl_bn_adversarial
     attack = Attack()
     attack(adv)
-    assert adv.image is not None
-    assert adv.distance.value < np.inf
+    assert adv.image is None
+    assert adv.distance.value == np.inf
 
 
 def test_targeted_attack(bn_targeted_adversarial):
