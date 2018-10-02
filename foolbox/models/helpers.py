20ecc7660ae41f5b9803eae9cccb0cc1d3917954
Jonas Rauber <git@jonasrauber.de>
2018-10-02 23:00:45 +0000

    fixed implemenation (basics working in python 3)

--- foolbox/models/helpers.py
+++ foolbox/models/helpers.py
@@ -1,4 +1,7 @@
+from __future__ import print_function
+
 import subprocess
+from subprocess import PIPE
 import os
 import sys
 from os.path import join
@@ -19,20 +22,30 @@ def from_git(url, path=None):
     Returns
     -------
     `foolbox.model.Model`
-        The model returned by the create_model
-        function.
+        The model returned by the create() function.
     """
 
     if path is None:
-        path = path.split('/')
+        path = url.split('/')
         path = path[-1] if path[-1] != '' else path[-2]
         path = path[:-4] if path.endswith('.git') else path
         path = './' + path
 
-    try:
-        subprocess.check_call(['git', 'clone', url, path])
-    except subprocess.CalledProcessError as e:
-        print('git clone failed: {}'.format(e))
+    if not os.path.exists(join(path, 'foolbox_model.py')):
+        cmd = ['git', 'clone', url, path]
+        print('running "{}"'.format(' '.join(cmd)))
+        p = subprocess.Popen(cmd, stdout=PIPE, stderr=PIPE)
+        output = p.communicate()
+        if p.returncode != 0:
+            print('"{}" failed with exit code {}'.format(
+                ' '.join(cmd), p.returncode))
+            stdout = output[0].decode("utf-8")
+            stderr = output[1].decode("utf-8")
+            if len(stdout) > 0:
+                print(stdout)
+            if len(stderr) > 0:
+                print(stderr)
+            return
 
     if not os.path.exists(join(path, 'foolbox_model.py')):
         raise ValueError('The repository does not contain a '
@@ -41,4 +54,4 @@ def from_git(url, path=None):
     sys.path.insert(0, path)
     module = importlib.import_module('foolbox_model')
     print('imported module: {}'.format(module))
-    return module.create_model()
+    return module.create()
