94120a0fdca7150c6aa99a038a535eb8b92c7464
Jonas Rauber <git@jonasrauber.de>
2018-10-02 22:30:47 +0000

    removed debugging statements and added absolute_import future to models

--- foolbox/models/tensorflow_eager.py
+++ foolbox/models/tensorflow_eager.py
@@ -37,7 +37,6 @@ class TensorFlowEagerModel(DifferentiableModel):
 
         # delay import until class is instantiated
         import tensorflow as tf
-        print(tf.__version__)
         assert tf.executing_eagerly()
 
         super(TensorFlowEagerModel, self).__init__(
--- foolbox/models/wrappers.py
+++ foolbox/models/wrappers.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 from .base import Model
 from .base import DifferentiableModel
 
