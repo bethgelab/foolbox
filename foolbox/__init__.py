48d822345d747552cc93868c7f725f3bf8d59401
Jonas Rauber <git@jonasrauber.de>
2018-10-23 20:57:54 +0000

    fixed import order

--- foolbox/__init__.py
+++ foolbox/__init__.py
@@ -3,13 +3,14 @@ from os.path import join, dirname
 with open(join(dirname(__file__), 'VERSION')) as f:
     __version__ = f.read().strip()
 
+from .rngs import rng  # noqa: F401
+from .rngs import nprng  # noqa: F401
+
 from . import models  # noqa: F401
 from . import criteria  # noqa: F401
 from . import distances  # noqa: F401
 from . import attacks  # noqa: F401
 from . import utils  # noqa: F401
 from . import gradient_estimators  # noqa: F401
-from .rngs import rng  # noqa: F401
-from .rngs import nprng  # noqa: F401
 
 from .adversarial import Adversarial  # noqa: F401
