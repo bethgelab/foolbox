985fa1ccadc837a2efb13cf6798e29de12186755
Wieland Brendel <wielandbrendel@users.noreply.github.com>
2018-12-22 11:00:00 +0100

    Adding test images for several popular dataset (#248)

--- foolbox/utils.py
+++ foolbox/utils.py
@@ -135,6 +135,7 @@ def imagenet_example(shape=(224, 224), data_format='channels_last'):
     label : int
         The imagenet label associated with the image.
 
+    NOTE: This function is deprecated and will be removed in the future.
     """
     assert len(shape) == 2
     assert data_format in ['channels_first', 'channels_last']
@@ -149,6 +150,68 @@ def imagenet_example(shape=(224, 224), data_format='channels_last'):
     if data_format == 'channels_first':
         image = np.transpose(image, (2, 0, 1))
     return image, 282
+
+
+def samples(dataset='imagenet', index=0, batchsize=1, shape=(224, 224),
+            data_format='channels_last'):
+    ''' Returns a batch of example images and the corresponding labels
+
+    Parameters
+    ----------
+    dataset : string
+        The data set to load (options: imagenet, mnist, cifar10,
+        cifar100, fashionMNIST)
+    index : int
+        For each data set 20 example images exist. The returned batch
+        contains the images with index [index, index + 1, index + 2, ...]
+    batchsize : int
+        Size of batch.
+    shape : list of integers
+        The shape of the returned image (only relevant for Imagenet).
+    data_format : str
+        "channels_first" or "channels_last"
+
+    Returns
+    -------
+    images : array_like
+        The batch of example images
+
+    labels : array of int
+        The labels associated with the images.
+
+    '''
+    from PIL import Image
+
+    images, labels = [], []
+    basepath = os.path.dirname(__file__)
+    samplepath = os.path.join(basepath, 'data')
+    files = os.listdir(samplepath)
+
+    for idx in range(index, index + batchsize):
+        i = idx % 20
+
+        # get filename and label
+        file = [n for n in files if '{}_{:02d}_'.format(dataset, i) in n][0]
+        label = int(file.split('.')[0].split('_')[-1])
+
+        # open file
+        path = os.path.join(samplepath, file)
+        image = Image.open(path)
+
+        if dataset == 'imagenet':
+            image = image.resize(shape)
+
+        image = np.asarray(image, dtype=np.float32)
+
+        if dataset is not 'mnist' and data_format == 'channels_first':
+            image = np.transpose(image, (2, 0, 1))
+
+        images.append(image)
+        labels.append(label)
+
+    labels = np.array(labels)
+    images = np.stack(images)
+    return images, labels
 
 
 def onehot_like(a, index, value=1):
