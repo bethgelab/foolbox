77c2b699daf0468bc1dc998cb57fd8027a8b4673
Jonas Rauber <git@jonasrauber.de>
2019-05-21 11:52:01 +0200

    updated CarliniWagnerL2Attack batch attack test

--- foolbox/tests/batch_attacks/test_batch_carlini_wagner.py
+++ foolbox/tests/batch_attacks/test_batch_carlini_wagner.py
@@ -1,35 +1,44 @@
 import numpy as np
+from contextlib import contextmanager
 
-from foolbox.attacks import CarliniWagnerL2Attack as Attack
-
-
-def test_untargeted_attack(bn_adversarial):
-    adv = bn_adversarial
-    attack = Attack()
-    attack(adv, max_iterations=100)
-    assert adv.perturbed is not None
-    assert adv.distance.value < np.inf
+from foolbox.batch_attacks import CarliniWagnerL2Attack as Attack
 
 
-def test_targeted_attack(bn_targeted_adversarial):
-    adv = bn_targeted_adversarial
-    attack = Attack()
-    attack(adv, max_iterations=100, binary_search_steps=20)
-    assert adv.perturbed is not None
-    assert adv.distance.value < np.inf
+def test_untargeted_attack(bn_model, bn_criterion, bn_images, bn_labels):
+    cm_model = contextmanager(bn_model)
+    with cm_model() as model:
+        attack = Attack(model, bn_criterion)
+        advs = attack(bn_images, bn_labels, unpack=False, max_iterations=100)
+    for adv in advs:
+        assert adv.perturbed is not None
+        assert adv.distance.value < np.inf
 
 
-def test_attack_impossible(bn_impossible):
-    adv = bn_impossible
-    attack = Attack()
-    attack(adv, max_iterations=100)
-    assert adv.perturbed is None
-    assert adv.distance.value == np.inf
+def test_targeted_attack(bn_model, bn_targeted_criterion, bn_images, bn_labels):
+    cm_model = contextmanager(bn_model)
+    with cm_model() as model:
+        attack = Attack(model, bn_targeted_criterion)
+        advs = attack(bn_images, bn_labels, unpack=False, max_iterations=100, binary_search_steps=20)
+    for adv in advs:
+        assert adv.perturbed is not None
+        assert adv.distance.value < np.inf
 
 
-def test_attack_gl(gl_bn_adversarial):
-    adv = gl_bn_adversarial
-    attack = Attack()
-    attack(adv, max_iterations=100)
-    assert adv.perturbed is None
-    assert adv.distance.value == np.inf
+def test_attack_impossible(bn_model, bn_impossible_criterion, bn_images, bn_labels):
+    cm_model = contextmanager(bn_model)
+    with cm_model() as model:
+        attack = Attack(model, bn_impossible_criterion)
+        advs = attack(bn_images, bn_labels, unpack=False, max_iterations=100, binary_search_steps=20)
+    for adv in advs:
+        assert adv.perturbed is None
+        assert adv.distance.value == np.inf
+
+
+def test_attack_gl(gl_bn_model, bn_criterion, bn_images, bn_labels):
+    cm_model = contextmanager(gl_bn_model)
+    with cm_model() as model:
+        attack = Attack(model, bn_criterion)
+        advs = attack(bn_images, bn_labels, unpack=False, max_iterations=100)
+    for adv in advs:
+        assert adv.perturbed is None
+        assert adv.distance.value == np.inf
