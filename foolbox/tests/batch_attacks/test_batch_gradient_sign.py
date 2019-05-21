fb2fcc4e2e2a2436396af6ed05228adb168d2402
Jonas Rauber <git@jonasrauber.de>
2019-05-21 15:33:06 +0200

    updated GradientSignAttack batch attack tests

--- foolbox/tests/batch_attacks/test_batch_gradient_sign.py
+++ foolbox/tests/batch_attacks/test_batch_gradient_sign.py
@@ -3,25 +3,25 @@ import numpy as np
 from foolbox.attacks import GradientSignAttack as Attack
 
 
-def test_attack(bn_adversarial):
-    adv = bn_adversarial
-    attack = Attack()
-    attack(adv)
-    assert adv.perturbed is not None
-    assert adv.distance.value < np.inf
+def test_untargeted_attack(bn_model, bn_criterion, bn_images, bn_labels):
+    attack = Attack(bn_model, bn_criterion)
+    advs = attack(bn_images, bn_labels, unpack=False)
+    for adv in advs:
+        assert adv.perturbed is not None
+        assert adv.distance.value < np.inf
 
 
-def test_attack_eps(bn_adversarial):
-    adv = bn_adversarial
-    attack = Attack()
-    attack(adv, epsilons=np.linspace(0., 1., 100)[1:])
-    assert adv.perturbed is not None
-    assert adv.distance.value < np.inf
+def test_attack_eps(bn_model, bn_criterion, bn_images, bn_labels):
+    attack = Attack(bn_model, bn_criterion)
+    advs = attack(bn_images, bn_labels, unpack=False, epsilons=np.linspace(0., 1., 100)[1:])
+    for adv in advs:
+        assert adv.perturbed is not None
+        assert adv.distance.value < np.inf
 
 
-def test_attack_gl(gl_bn_adversarial):
-    adv = gl_bn_adversarial
-    attack = Attack()
-    attack(adv)
-    assert adv.perturbed is None
-    assert adv.distance.value == np.inf
+def test_attack_gl(gl_bn_model, bn_criterion, bn_images, bn_labels):
+    attack = Attack(gl_bn_model, bn_criterion)
+    advs = attack(bn_images, bn_labels, unpack=False)
+    for adv in advs:
+        assert adv.perturbed is None
+        assert adv.distance.value == np.inf
