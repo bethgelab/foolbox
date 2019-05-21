f336ca3716dd5ecb14525888cf4a1b8e24b41c64
Jonas Rauber <git@jonasrauber.de>
2019-05-21 11:52:25 +0200

    updated GradientAttack batch attack test

--- foolbox/tests/batch_attacks/test_batch_gradient.py
+++ foolbox/tests/batch_attacks/test_batch_gradient.py
@@ -1,35 +1,65 @@
+import pytest
 import numpy as np
+from contextlib import contextmanager
 
-from foolbox.attacks import GradientAttack as Attack
+from foolbox.gradient_estimators import CoordinateWiseGradientEstimator
+from foolbox.gradient_estimators import EvolutionaryStrategiesGradientEstimator
 
+from foolbox.models import ModelWithEstimatedGradients
 
-def test_attack(bn_adversarial):
-    adv = bn_adversarial
-    attack = Attack()
-    attack(adv)
-    assert adv.perturbed is not None
-    assert adv.distance.value < np.inf
+from foolbox.batch_attacks import GradientAttack as Attack
 
 
-def test_attack_eps(bn_adversarial):
-    adv = bn_adversarial
-    attack = Attack()
-    attack(adv, epsilons=np.linspace(0., 1., 100)[1:])
-    assert adv.perturbed is not None
-    assert adv.distance.value < np.inf
+def test_untargeted_attack(bn_model, bn_criterion, bn_images, bn_labels):
+    cm_model = contextmanager(bn_model)
+    with cm_model() as model:
+        attack = Attack(model, bn_criterion)
+        advs = attack(bn_images, bn_labels, unpack=False)
+    for adv in advs:
+        assert adv.perturbed is not None
+        assert adv.distance.value < np.inf
 
 
-def test_attack_gl(gl_bn_adversarial):
-    adv = gl_bn_adversarial
-    attack = Attack()
-    attack(adv)
-    assert adv.perturbed is None
-    assert adv.distance.value == np.inf
+def test_attack_eps(bn_model, bn_criterion, bn_images, bn_labels):
+    cm_model = contextmanager(bn_model)
+    with cm_model() as model:
+        attack = Attack(model, bn_criterion)
+        advs = attack(bn_images, bn_labels, unpack=False, epsilons=np.linspace(0., 1., 100)[1:])
+    for adv in advs:
+        assert adv.perturbed is not None
+        assert adv.distance.value < np.inf
 
 
-def test_attack_eg(eg_bn_adversarial):
-    adv = eg_bn_adversarial
-    attack = Attack()
-    attack(adv)
-    assert adv.perturbed is not None
-    assert adv.distance.value < np.inf
+def test_attack_gl(gl_bn_model, bn_criterion, bn_images, bn_labels):
+    cm_model = contextmanager(gl_bn_model)
+    with cm_model() as model:
+        attack = Attack(model, bn_criterion)
+        advs = attack(bn_images, bn_labels, unpack=False)
+    for adv in advs:
+        assert adv.perturbed is None
+        assert adv.distance.value == np.inf
+
+
+def eg_bn_model_factory(request, bn_model):
+    GradientEstimator = request.param
+
+    def eg_bn_model():
+        cm_model = contextmanager(bn_model)
+        with cm_model() as model:
+            gradient_estimator = GradientEstimator(epsilon=0.01)
+            model = ModelWithEstimatedGradients(model, gradient_estimator)
+            yield model
+    return eg_bn_model
+
+
+@pytest.fixture(params=[CoordinateWiseGradientEstimator,
+                        EvolutionaryStrategiesGradientEstimator])
+def test_attack_eg(request, bn_model, bn_criterion, bn_images, bn_labels):
+    eg_bn_model = eg_bn_model_factory(request, bn_model)
+    cm_model = contextmanager(eg_bn_model)
+    with cm_model() as model:
+        attack = Attack(model, bn_criterion)
+        advs = attack(bn_images, bn_labels, unpack=False)
+    for adv in advs:
+        assert adv.perturbed is not None
+        assert adv.distance.value < np.inf
