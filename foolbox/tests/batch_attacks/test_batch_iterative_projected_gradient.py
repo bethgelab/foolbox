0dc838718ac58e7ac1476f775dc54a2271db6c86
FlashTek <rzrolandzimmermann@gmail.com>
2019-10-17 10:16:36 +0200

    Fix formatting

--- foolbox/tests/batch_attacks/test_batch_iterative_projected_gradient.py
+++ foolbox/tests/batch_attacks/test_batch_iterative_projected_gradient.py
@@ -24,7 +24,7 @@ Attacks = [
     ProjectedGradientDescentAttack,
     RandomStartProjectedGradientDescentAttack,
     MomentumIterativeAttack,
-    SparseL1BasicIterativeAttack
+    SparseL1BasicIterativeAttack,
     AdamL1BasicIterativeAttack,
     AdamL2BasicIterativeAttack,
     AdamProjectedGradientDescentAttack,
