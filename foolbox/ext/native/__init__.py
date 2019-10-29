565a593f3005b8c319e01f885bfd567d652abd0d
Jonas Rauber <git@jonasrauber.de>
2019-10-29 14:56:00 +0100

    clean namespace

--- foolbox/ext/native/__init__.py
+++ foolbox/ext/native/__init__.py
@@ -1,7 +1,8 @@
-from os.path import join, dirname
+from os.path import join as _join
+from os.path import dirname as _dirname
 
-with open(join(dirname(__file__), "VERSION")) as f:
-    __version__ = f.read().strip()
+with open(_join(_dirname(__file__), "VERSION")) as _f:
+    __version__ = _f.read().strip()
 
 
 from . import models  # noqa: F401
