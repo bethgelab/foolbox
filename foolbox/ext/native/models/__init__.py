80e657ab7e16e71f7ce9cef0dcf643054b77f726
Jonas Rauber <git@jonasrauber.de>
2019-10-29 15:07:19 +0100

    added FoolboxModel to wrap classic Foolbox Models in a future-proof way

--- foolbox/ext/native/models/__init__.py
+++ foolbox/ext/native/models/__init__.py
@@ -1,2 +1,3 @@
 from .pytorch import PyTorchModel  # noqa: F401
 from .tensorflow import TensorFlowModel  # noqa: F401
+from .foolbox import FoolboxModel  # noqa: F401
---
+++ foolbox/ext/native/models/foolbox.py
@@ -0,0 +1,16 @@
+import foolbox
+
+
+class FoolboxModel:
+    def __init__(self, model):
+        assert isinstance(model, foolbox.models.base.Model)
+        self._model = model
+
+    def bounds(self):
+        return self._model.bounds()
+
+    def forward(self, inputs):
+        return self._model.forward(inputs)
+
+    def gradient(self, inputs, labels):
+        return self._model.gradient(inputs, labels)
