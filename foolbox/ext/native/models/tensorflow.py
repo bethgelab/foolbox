fef8c9c6dd53d6820d2c49982873f792634a1302
Jonas Rauber <git@jonasrauber.de>
2019-10-29 16:26:37 +0100

    pre-compute tensorflow preprocessing

--- foolbox/ext/native/models/tensorflow.py
+++ foolbox/ext/native/models/tensorflow.py
@@ -2,38 +2,48 @@ import tensorflow as tf
 
 
 class TensorFlowModel:
-    def __init__(self, model, bounds, preprocessing=dict(mean=0, std=1)):
+    def __init__(self, model, bounds, preprocessing=None):
         assert tf.executing_eagerly()
-        assert set(preprocessing.keys()) - {"mean", "std", "axis"} == set()
         self._bounds = bounds
-        self._preprocessing = preprocessing
         self._model = model
+        self._init_preprocessing(preprocessing)
 
-    def _preprocess(self, inputs):
-        x = inputs
-
-        mean = self._preprocessing.get("mean", 0)
-        std = self._preprocessing.get("std", 1)
+    def _init_preprocessing(self, preprocessing):
+        assert set(preprocessing.keys()) - {"mean", "std", "axis"} == set()
+        mean = self._preprocessing.get("mean", None)
+        std = self._preprocessing.get("std", None)
         axis = self._preprocessing.get("axis", None)
 
-        if axis is not None:
+        if mean is not None:
             mean = tf.convert_to_tensor(mean)
+        if std is not None:
             std = tf.convert_to_tensor(std)
-            assert mean.ndim == 1, "If axis is specified, mean should be 1-dimensional"
-            assert std.ndim == 1, "If axis is specified, std should be 1-dimensional"
+
+        if axis is not None:
             assert (
                 axis < 0
             ), "axis must be negative integer, with -1 representing the last axis"
-            s = (1,) * (abs(axis) - 1)
-            mean = tf.reshape(mean, mean.shape + s)
-            std = tf.reshape(std, std.shape + s)
+            shape = (1,) * (abs(axis) - 1)
+            if mean is not None:
+                assert (
+                    mean.ndim == 1
+                ), "If axis is specified, mean should be 1-dimensional"
+                mean = tf.reshape(mean, mean.shape + shape)
+            if std is not None:
+                assert (
+                    std.ndim == 1
+                ), "If axis is specified, std should be 1-dimensional"
+                std = tf.reshape(std, std.shape + shape)
 
-        if isinstance(mean, tf.Tensor) or mean != 0:
-            x = x - mean
+        self._preprocessing_mean = mean
+        self._preprocessing_std = std
 
-        if isinstance(std, tf.tensor) or std != 1:
-            x = x / std
-
+    def _preprocess(self, inputs):
+        x = inputs
+        if self._preprocessing_mean is not None:
+            x = x - self._preprocessing_mean
+        if self._preprocessing_std is not None:
+            x = x / self._preprocessing_std
         assert x.dtype == inputs.dtype
         return x
 
