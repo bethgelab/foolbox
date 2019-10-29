53298e6701ffa1fa1a4cc6f2a1c6a6e50f05c962
Jonas Rauber <git@jonasrauber.de>
2019-10-29 11:52:57 +0100

    updated tests

--- tests/test_pytorch_model.py
+++ tests/test_pytorch_model.py
@@ -1,14 +1,15 @@
+import pytest
 import numpy as np
 import torch
 import torch.nn as nn
 
 from foolbox.ext.native.models import PyTorchModel
+from foolbox.ext.native.attacks import LinfinityBasicIterativeAttack
+from foolbox.ext.native.attacks import L2BasicIterativeAttack
 
 
-def test_pytorch_model():
-    channels = num_classes = 10
-    batch_size = 8
-    h = w = 32
+@pytest.fixture
+def fmodel():
     bounds = (0, 1)
 
     class Model(nn.Module):
@@ -19,17 +20,65 @@ def test_pytorch_model():
 
     model = Model()
     fmodel = PyTorchModel(model, bounds=bounds)
+    return fmodel
+
+
+@pytest.fixture
+def fmodel_and_data(fmodel):
+    channels = num_classes = 10
+    batch_size = 8
+    h = w = 32
 
     np.random.seed(0)
-    x = np.random.uniform(*bounds, size=(batch_size, channels, h, w)).astype(np.float32)
+    x = np.random.uniform(*fmodel.bounds(), size=(batch_size, channels, h, w)).astype(
+        np.float32
+    )
     x = torch.from_numpy(x).to(fmodel.device)
+
     y = np.arange(batch_size) % num_classes
     y = torch.from_numpy(y).to(fmodel.device)
+    return fmodel, x, y, batch_size, num_classes
 
+
+def test_pytorch_model_forward(fmodel_and_data):
+    fmodel, x, y, batch_size, num_classes = fmodel_and_data
     output = fmodel.forward(x)
     assert output.shape == (batch_size, num_classes)
     assert isinstance(output, torch.Tensor)
 
+
+def test_pytorch_model_gradient(fmodel_and_data):
+    fmodel, x, y, batch_size, num_classes = fmodel_and_data
     output = fmodel.gradient(x, y)
     assert output.shape == x.shape
     assert isinstance(output, torch.Tensor)
+
+
+def test_pytorch_linf_basic_iterative_attack(fmodel_and_data):
+    fmodel, x, y, batch_size, num_classes = fmodel_and_data
+    y = fmodel.forward(x).argmax(axis=-1)
+
+    attack = LinfinityBasicIterativeAttack(fmodel)
+    advs = attack(x, y, rescale=False, epsilon=0.3)
+
+    perturbation = advs - x
+    y_advs = fmodel.forward(advs).argmax(axis=-1)
+
+    assert x.shape == advs.shape
+    assert perturbation.abs().max() <= 0.3 + 1e7
+    assert (y_advs == y).float().mean() < 1
+
+
+def test_pytorch_l2_basic_iterative_attack(fmodel_and_data):
+    fmodel, x, y, batch_size, num_classes = fmodel_and_data
+    y = fmodel.forward(x).argmax(axis=-1)
+
+    attack = L2BasicIterativeAttack(fmodel)
+    advs = attack(x, y, rescale=False, epsilon=0.3)
+
+    perturbation = advs - x
+    y_advs = fmodel.forward(advs).argmax(axis=-1)
+
+    assert x.shape == advs.shape
+    assert perturbation.abs().max() <= 0.3 + 1e7
+    assert (y_advs == y).float().mean() < 1
