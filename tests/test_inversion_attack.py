b90aee9dc2a5219ea91c61927c435fdce42f1130
zimmerrol <rzrolandzimmermann@gmail.com>
2019-12-11 10:28:10 +0100

    Add inversion attack

---
+++ tests/test_inversion_attack.py
@@ -0,0 +1,37 @@
+import eagerpy as ep
+import numpy as np
+import torch
+import torch.nn as nn
+
+from foolbox.ext.native.utils import flatten
+from foolbox.ext.native.models import PyTorchModel
+from foolbox.ext.native.attacks import InversionAttack
+
+
+def test_inversion_attack():
+    channels = 3
+    batch_size = 8
+    h = w = 32
+    bounds = (0, 1)
+
+    class Model(nn.Module):
+        def forward(self, x):
+            x = torch.mean(x, 3)
+            x = torch.mean(x, 2)
+            return x
+
+    model = Model().eval()
+    fmodel = PyTorchModel(model, bounds=bounds)
+
+    np.random.seed(0)
+    x = np.random.uniform(*bounds, size=(batch_size, channels, h, w)).astype(np.float32)
+    x = torch.from_numpy(x).to(fmodel.device)
+    y = fmodel.forward(x).argmax(axis=-1)
+
+    attack = InversionAttack(fmodel)
+    advs = attack(x, y)
+
+    y_advs = fmodel.forward(advs).argmax(axis=-1)
+
+    assert x.shape == advs.shape
+    assert (y_advs == y).float().mean() < 1
