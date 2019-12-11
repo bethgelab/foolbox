90ce31b998e1836bfe5eb97c272a0e15630828a0
zimmerrol <rzrolandzimmermann@gmail.com>
2019-12-11 16:39:01 +0100

    Add EAD attack

--- foolbox/ext/native/attacks/__init__.py
+++ foolbox/ext/native/attacks/__init__.py
@@ -3,6 +3,7 @@ from .basic_iterative_method import LinfinityBasicIterativeAttack  # noqa: F401
 from .fast_gradient_method import L2FastGradientAttack  # noqa: F401
 from .fast_gradient_method import LinfinityFastGradientAttack  # noqa: F401
 from .carlini_wagner import L2CarliniWagnerAttack  # noqa: F401
+from .ead import EADAttack
 from .projected_gradient_descent import ProjectedGradientDescentAttack  # noqa: F401
 from .contrast import L2ContrastReductionAttack  # noqa: F401
 from .contrast import BinarySearchContrastReductionAttack  # noqa: F401
