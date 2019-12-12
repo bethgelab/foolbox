d64b57fc3f0062c029d3a1f6865cbb3baaf367f2
Jonas Rauber <git@jonasrauber.de>
2019-12-12 13:31:08 +0100

    added LinearSearchBlendedUniformNoiseAttack

--- foolbox/ext/native/attacks/__init__.py
+++ foolbox/ext/native/attacks/__init__.py
@@ -3,12 +3,13 @@ from .basic_iterative_method import LinfinityBasicIterativeAttack  # noqa: F401
 from .fast_gradient_method import L2FastGradientAttack  # noqa: F401
 from .fast_gradient_method import LinfinityFastGradientAttack  # noqa: F401
 from .carlini_wagner import L2CarliniWagnerAttack  # noqa: F401
-from .ead import EADAttack
+from .ead import EADAttack  # noqa: F401
 from .projected_gradient_descent import ProjectedGradientDescentAttack  # noqa: F401
 from .contrast import L2ContrastReductionAttack  # noqa: F401
 from .contrast import BinarySearchContrastReductionAttack  # noqa: F401
 from .contrast import LinearSearchContrastReductionAttack  # noqa: F401
 from .inversion import InversionAttack  # noqa: F401
+from .blended_noise import LinearSearchBlendedUniformNoiseAttack  # noqa: F401
 
 FGM = L2FastGradientAttack
 FGSM = LinfinityFastGradientAttack
