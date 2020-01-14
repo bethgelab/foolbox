86cc70e899f9472319a41d98f28b6bda0ea9b058
Jonas Rauber <git@jonasrauber.de>
2020-01-14 15:17:17 +0100

    applied black to precomputed_samples

--- foolbox/ext/native/attacks/precomputed_samples.py
+++ foolbox/ext/native/attacks/precomputed_samples.py
@@ -2,6 +2,7 @@
 import eagerpy as ep
 import numpy as np
 
+
 class PrecomputedSamplesAttack:
     """This is a helper attack that makes it straight-forward to choose initialisation points
        for boundary-type attacks from a given data set. All it does is to store samples and
@@ -13,10 +14,10 @@ class PrecomputedSamplesAttack:
         self.model = model
         self.samples = []
         self.labels = []
-        
+
     def feed(self, inputs):
         response = self.model.forward(inputs).argmax(1)
-        
+
         for k in range(len(inputs)):
             self.labels.append(int(response[k]))
             self.samples.append(inputs[k])
@@ -25,15 +26,15 @@ class PrecomputedSamplesAttack:
         inputs = ep.astensor(inputs)
         labels = ep.astensor(labels)
         x = ep.zeros_like(inputs)
-        
+
         adv_samples = []
-        
+
         for k in range(len(labels)):
             while True:
                 idx = np.random.randint(len(self.labels))
                 if int(labels[k].numpy()) != self.labels[idx]:
                     adv_samples.append(ep.astensor(self.samples[idx]).numpy())
                     break
-                    
+
         x = ep.from_numpy(x, np.stack(adv_samples))
         return x.tensor
