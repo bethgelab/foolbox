98616a9eea5f871f2fd74ce4aa4c93fdcd45d335
Jonas Rauber <git@jonasrauber.de>
2020-01-15 15:42:02 +0100

    use eagerpy 0.11.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except IOError:
     README = ""
 
 
-install_requires = ["numpy", "scipy", "setuptools", "foolbox>=2.2.1", "eagerpy>=0.10.0"]
+install_requires = ["numpy", "scipy", "setuptools", "foolbox>=2.2.1", "eagerpy>=0.11.0"]
 tests_require = ["pytest"]
 
 
