af797d2a50e11d224710ec1ad5c6f6be5356b240
Jonas Rauber <git@jonasrauber.de>
2020-01-17 14:18:54 +0100

    expose distances

--- foolbox/ext/native/__init__.py
+++ foolbox/ext/native/__init__.py
@@ -7,9 +7,9 @@ with open(_join(_dirname(__file__), "VERSION")) as _f:
 
 from . import models  # noqa: F401
 from . import attacks  # noqa: F401
+from . import distances  # noqa: F401
 from . import utils  # noqa: F401
 from . import devutils  # noqa: F401
 from . import plot  # noqa: F401
-from . import norms  # noqa: F401
 from . import tensorboard  # noqa: F401
 from .evaluate import evaluate_l2  # noqa: F401
