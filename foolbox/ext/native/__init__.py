64d6b954d6cdc3b3ff69d8f0bd5422b8191c8a35
Jonas Rauber <git@jonasrauber.de>
2020-01-25 20:21:26 +0100

    removed unnessary newline

--- foolbox/ext/native/__init__.py
+++ foolbox/ext/native/__init__.py
@@ -4,7 +4,6 @@ from os.path import dirname as _dirname
 with open(_join(_dirname(__file__), "VERSION")) as _f:
     __version__ = _f.read().strip()
 
-
 from . import models  # noqa: F401
 from . import attacks  # noqa: F401
 from . import distances  # noqa: F401
