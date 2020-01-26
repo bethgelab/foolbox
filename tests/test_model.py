943b4f90f04c6bc2451a10791ef1077d3d27b802
Jonas Rauber <git@jonasrauber.de>
2020-01-26 02:38:44 +0100

    additional test for model (un)wrapping

--- tests/test_model.py
+++ tests/test_model.py
@@ -9,9 +9,21 @@ def test_bounds(fmodel_and_data):
     assert (x <= max_).all()
 
 
-def test_forward(fmodel_and_data):
+def test_forward_unwrapped(fmodel_and_data):
     fmodel, x, y = fmodel_and_data
     logits = ep.astensor(fmodel.forward(x.tensor))
+    assert logits.ndim == 2
+    assert len(logits) == len(x) == len(y)
+    _, num_classes = logits.shape
+    assert (y >= 0).all()
+    assert (y < num_classes).all()
+
+
+def test_forward_wrapped(fmodel_and_data):
+    fmodel, x, y = fmodel_and_data
+    assert ep.istensor(x)
+    logits = fmodel.forward(x)
+    assert ep.istensor(logits)
     assert logits.ndim == 2
     assert len(logits) == len(x) == len(y)
     _, num_classes = logits.shape
