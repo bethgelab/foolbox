2e7f8aa7438289f2d83d588a22a8b3f8524a9eba
Jonas Rauber <git@jonasrauber.de>
2020-02-01 15:20:45 +0100

    updated test_attacks

--- tests/attacks/incomplete/test_attacks.py
+++ tests/attacks/incomplete/test_attacks.py
@@ -1,20 +1,16 @@
+from typing import List, Tuple
 import pytest
-import foolbox.ext.native as fbn
-from typing import List, Tuple, Type, Dict, Any
+import eagerpy as ep
 
-attacks: List[Tuple[Type[fbn.attacks.Attack], Dict[str, Any], Dict[str, Any]]] = [
-    (fbn.attacks.InversionAttack, {}, {}),
-    # (fbn.attacks.L2AdditiveGaussianNoiseAttack, {}, dict(epsilon=2000.0)),  # currently, this epsilon is absolute
-    # (fbn.attacks.L2AdditiveUniformNoiseAttack, {}, dict(epsilon=2000.0)),  # (l2 norm of data between the bounds)
-    # (fbn.attacks.LinfAdditiveUniformNoiseAttack, {}, dict(epsilon=100.0)),
-    # (fbn.attacks.L2BasicIterativeAttack, {}, dict(epsilon=2000.0)),
-]
+import foolbox.ext.native as fbn
+
+attacks: List[fbn.Attack] = [fbn.attacks.InversionAttack()]
 
 
 @pytest.mark.parametrize("attack", attacks)
-def test_callable(fmodel_and_data_for_attacks, attack):
-    fmodel, x, y = fmodel_and_data_for_attacks
-    attack, init_kwargs, call_kwargs = attack
-    attack = attack(fmodel, **init_kwargs)
-    advs = attack(x, y, **call_kwargs)
+def test_init_and_call(
+    fmodel_and_data: Tuple[fbn.Model, ep.Tensor, ep.Tensor], attack: fbn.Attack
+) -> None:
+    fmodel, x, y = fmodel_and_data
+    advs = attack(fmodel, x, y)
     assert fbn.accuracy(fmodel, advs, y) < fbn.accuracy(fmodel, x, y)
