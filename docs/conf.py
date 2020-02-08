6fd6f0b3e363341553442c5256f780e44673ab13
Jonas Rauber <git@jonasrauber.de>
2020-02-08 18:28:02 +0100

    updated readthedocs.yml to to v2 and added foolbox version to sphinx conf

--- docs/conf.py
+++ docs/conf.py
@@ -14,12 +14,17 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import foolbox
+
 
 # -- Project information -----------------------------------------------------
 
-project = 'Foolbox'
-copyright = '2020, Jonas Rauber'
-author = 'Jonas Rauber'
+project = "Foolbox"
+copyright = "2020, Jonas Rauber"
+author = "Jonas Rauber"
+
+version = foolbox.__version__
+release = foolbox.__version__
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,16 +32,15 @@ author = 'Jonas Rauber'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -44,10 +48,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 # html_theme = 'sphinx_typlog_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
