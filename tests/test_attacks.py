c5b2f6a626e10386a1364e3421893fa644e32d76
Jonas Rauber <git@jonasrauber.de>
2020-02-10 20:50:40 +0100

    added the ability to test certain attacks only on real models

--- tests/test_attacks.py
+++ tests/test_attacks.py
@@ -3,43 +3,48 @@ import pytest
 import eagerpy as ep
 
 import foolbox as fbn
+import foolbox.attacks as fa
 
 L2 = fbn.types.L2
 Linf = fbn.types.Linf
 
 
-attacks: List[Tuple[fbn.Attack, bool]] = [
-    (fbn.attacks.DDNAttack(), True),
-    (fbn.attacks.DDNAttack(rescale=True), True),
-    (fbn.attacks.InversionAttack(), False),
-    (fbn.attacks.L2ContrastReductionAttack(L2(100.0)), False),
-    (fbn.attacks.BinarySearchContrastReductionAttack(binary_search_steps=15), False),
-    (fbn.attacks.LinearSearchContrastReductionAttack(steps=20), False),
-    (fbn.attacks.L2CarliniWagnerAttack(binary_search_steps=3, steps=20), True),
-    (fbn.attacks.EADAttack(binary_search_steps=3, steps=20), True),
-    (fbn.attacks.EADAttack(binary_search_steps=3, steps=20, decision_rule="L1"), True),
-    (fbn.attacks.NewtonFoolAttack(steps=20), True),
-    (fbn.attacks.L2ContrastReductionAttack(L2(100.0)).repeat(3), False),
-    (fbn.attacks.VirtualAdversarialAttack(iterations=50, xi=1, epsilon=10), True),
-    (fbn.attacks.L2BasicIterativeAttack(L2(100.0), stepsize=5.0, steps=10), True),
-    (fbn.attacks.LinfBasicIterativeAttack(Linf(1.0), stepsize=5.0, steps=10), True),
+# attack, attack_uses_grad, requires_real_model
+attacks: List[Tuple[fbn.Attack, bool, bool]] = [
+    (fa.DDNAttack(), True, False),
+    (fa.DDNAttack(rescale=True), True, False),
+    (fa.InversionAttack(), False, False),
+    (fa.L2ContrastReductionAttack(L2(100.0)), False, False),
+    (fa.BinarySearchContrastReductionAttack(binary_search_steps=15), False, False),
+    (fa.LinearSearchContrastReductionAttack(steps=20), False, False),
+    (fa.L2CarliniWagnerAttack(binary_search_steps=3, steps=20), True, False),
+    (fa.EADAttack(binary_search_steps=3, steps=20), True, False),
+    (fa.EADAttack(binary_search_steps=3, steps=20, decision_rule="L1"), True, False),
+    (fa.NewtonFoolAttack(steps=20), True, False),
+    (fa.L2ContrastReductionAttack(L2(100.0)).repeat(3), False, False),
+    (fa.VirtualAdversarialAttack(iterations=50, xi=1, epsilon=10), True, False),
+    (fa.L2BasicIterativeAttack(L2(100.0), stepsize=5.0, steps=10), True, False),
+    (fa.LinfBasicIterativeAttack(Linf(1.0), stepsize=5.0, steps=10), True, False),
     (
-        fbn.attacks.ProjectedGradientDescentAttack(Linf(1.0), stepsize=5.0, steps=10),
+        fa.ProjectedGradientDescentAttack(Linf(1.0), stepsize=5.0, steps=10),
         True,
+        False,
     ),
-    (fbn.attacks.L2FastGradientAttack(L2(100.0)), True),
-    (fbn.attacks.LinfFastGradientAttack(Linf(100.0)), True),
+    (fa.L2FastGradientAttack(L2(100.0)), True, False),
+    (fa.LinfFastGradientAttack(Linf(100.0)), True, False),
 ]
 
 
 @pytest.mark.parametrize("attack_and_grad", attacks)
 def test_untargeted_attacks(
-    fmodel_and_data: Tuple[fbn.Model, ep.Tensor, ep.Tensor],
-    attack_and_grad: Tuple[fbn.Attack, bool],
+    fmodel_and_data_ext: Tuple[Tuple[fbn.Model, ep.Tensor, ep.Tensor], bool],
+    attack_and_grad: Tuple[fbn.Attack, bool, bool],
 ) -> None:
 
-    attack, attack_uses_grad = attack_and_grad
-    fmodel, x, y = fmodel_and_data
+    attack, attack_uses_grad, requires_real_model = attack_and_grad
+    (fmodel, x, y), real = fmodel_and_data_ext
+    if requires_real_model and not real:
+        pytest.skip()
 
     if isinstance(x, ep.NumPyTensor) and attack_uses_grad:
         pytest.skip()
@@ -51,26 +56,29 @@ def test_untargeted_attacks(
     assert fbn.accuracy(fmodel, advs, y) < fbn.accuracy(fmodel, x, y)
 
 
-targeted_attacks: List[Tuple[fbn.Attack, bool]] = [
-    (fbn.attacks.L2CarliniWagnerAttack(binary_search_steps=3, steps=20), True),
-    (fbn.attacks.DDNAttack(), True),
+targeted_attacks: List[Tuple[fbn.Attack, bool, bool]] = [
+    (fa.L2CarliniWagnerAttack(binary_search_steps=3, steps=20), True, False),
+    (fa.DDNAttack(), True, False),
     (
-        fbn.attacks.EADAttack(
+        fa.EADAttack(
             binary_search_steps=3, steps=20, abort_early=True, regularization=0
         ),
         True,
+        False,
     ),
 ]
 
 
 @pytest.mark.parametrize("attack_and_grad", targeted_attacks)
 def test_targeted_attacks(
-    fmodel_and_data: Tuple[fbn.Model, ep.Tensor, ep.Tensor],
-    attack_and_grad: Tuple[fbn.Attack, bool],
+    fmodel_and_data_ext: Tuple[Tuple[fbn.Model, ep.Tensor, ep.Tensor], bool],
+    attack_and_grad: Tuple[fbn.Attack, bool, bool],
 ) -> None:
 
-    attack, attack_uses_grad = attack_and_grad
-    fmodel, x, y = fmodel_and_data
+    attack, attack_uses_grad, requires_real_model = attack_and_grad
+    (fmodel, x, y), real = fmodel_and_data_ext
+    if requires_real_model and not real:
+        pytest.skip()
 
     if isinstance(x, ep.NumPyTensor) and attack_uses_grad:
         pytest.skip()
