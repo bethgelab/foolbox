927b52704bd890c4682d6b46b13e39b1895456ee
Roland Zimmermann <5895436+zimmerrol@users.noreply.github.com>
2020-02-14 16:03:01 +0100

    Port the Brendel Bethge Attack to latest API (#470)
    * First step of port to latest fb version
    
    * Fix mypy issues
    
    * Expand unit tests
    
    * Simplify unit tests
    
    * Remove masking from attack
    
    * Extend unit tests
    
    * Update to #427 API
    
    * Fix unit test
    
    * Fix type annotation
    
    * Fix mypy issue
    
    * Compare lp metrics properly
    
    * Relax unit tests

--- foolbox/attacks/__init__.py
+++ foolbox/attacks/__init__.py
@@ -41,6 +41,12 @@ from .blended_noise import LinearSearchBlendedUniformNoiseAttack  # noqa: F401
 from .binarization import BinarizationRefinementAttack  # noqa: F401
 from .dataset_attack import DatasetAttack  # noqa: F401
 from .boundary_attack import BoundaryAttack  # noqa: F401
+from .brendel_bethge import (  # noqa: F401
+    L0BrendelBethgeAttack,
+    L1BrendelBethgeAttack,
+    L2BrendelBethgeAttack,
+    LinfinityBrendelBethgeAttack,
+)
 
 # from .blended_noise import LinearSearchBlendedUniformNoiseAttack  # noqa: F401
 # from .brendel_bethge import (  # noqa: F401
---
+++ tests/test_brendel_bethge_attack.py
@@ -0,0 +1,49 @@
+from typing import Tuple, Union, List
+import eagerpy as ep
+
+import foolbox as fbn
+import foolbox.attacks as fa
+from foolbox.devutils import flatten
+from foolbox.attacks.brendel_bethge import BrendelBethgeAttack
+import pytest
+
+
+def get_attack_id(x: Tuple[BrendelBethgeAttack, Union[int, float]]) -> str:
+    return repr(x[0])
+
+
+attacks: List[Tuple[fa.Attack, Union[int, float]]] = [
+    (fa.L0BrendelBethgeAttack(steps=50), 0),
+    (fa.L1BrendelBethgeAttack(steps=50), 1),
+    (fa.L2BrendelBethgeAttack(steps=50), 2),
+    (fa.LinfinityBrendelBethgeAttack(steps=50), ep.inf),
+]
+
+
+@pytest.mark.parametrize("attack_and_p", attacks, ids=get_attack_id)
+def test_brendel_bethge_untargeted_attack(
+    fmodel_and_data_ext_for_attacks: Tuple[
+        Tuple[fbn.Model, ep.Tensor, ep.Tensor], bool
+    ],
+    attack_and_p: Tuple[BrendelBethgeAttack, Union[int, float]],
+) -> None:
+    (fmodel, x, y), real = fmodel_and_data_ext_for_attacks
+
+    if isinstance(x, ep.NumPyTensor):
+        pytest.skip()
+
+    x = (x - fmodel.bounds.lower) / (fmodel.bounds.upper - fmodel.bounds.lower)
+    fmodel = fmodel.transform_bounds((0, 1))
+
+    init_attack = fa.LinearSearchBlendedUniformNoiseAttack(directions=100, steps=10)
+    init_advs = init_attack.run(fmodel, x, y)
+
+    attack, p = attack_and_p
+    advs = attack.run(fmodel, x, y, starting_points=init_advs)
+
+    init_norms = ep.norms.lp(flatten(init_advs - x), p=p, axis=-1)
+    norms = ep.norms.lp(flatten(advs - x), p=p, axis=-1)
+
+    assert fbn.accuracy(fmodel, advs, y) < fbn.accuracy(fmodel, x, y)
+    assert fbn.accuracy(fmodel, advs, y) <= fbn.accuracy(fmodel, init_advs, y)
+    assert (norms < init_norms).any()
