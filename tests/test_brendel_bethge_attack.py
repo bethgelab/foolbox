fcd739e46491a8d0f4e2b244de1726cca6b98ccf
zimmerrol <rzrolandzimmermann@gmail.com>
2020-02-18 14:08:24 +0100

    Extend unit tests for BrendelBethge attack

--- tests/test_brendel_bethge_attack.py
+++ tests/test_brendel_bethge_attack.py
@@ -50,3 +50,84 @@ def test_brendel_bethge_untargeted_attack(
     assert fbn.accuracy(fmodel, advs, y) < fbn.accuracy(fmodel, x, y)
     assert fbn.accuracy(fmodel, advs, y) <= fbn.accuracy(fmodel, init_advs, y)
     assert is_smaller.any()
+
+
+@pytest.mark.parametrize("attack_and_p", attacks, ids=get_attack_id)
+def test_brendel_bethge_targeted_attack(
+    fmodel_and_data_ext_for_attacks: Tuple[
+        Tuple[fbn.Model, ep.Tensor, ep.Tensor], bool
+    ],
+    attack_and_p: Tuple[BrendelBethgeAttack, Union[int, float]],
+) -> None:
+    (fmodel, x, y), real = fmodel_and_data_ext_for_attacks
+
+    if isinstance(x, ep.NumPyTensor):
+        pytest.skip()
+
+    x = (x - fmodel.bounds.lower) / (fmodel.bounds.upper - fmodel.bounds.lower)
+    fmodel = fmodel.transform_bounds((0, 1))
+
+    num_classes = fmodel(x).shape[-1]
+    target_classes = (y + 1) % num_classes
+    criterion = fbn.TargetedMisclassification(target_classes)
+    adv_before_attack = criterion(x, fmodel(x))
+    assert not adv_before_attack.all()
+
+    init_attack = fa.DatasetAttack()
+    init_attack.feed(fmodel, x)
+    init_advs = init_attack.run(fmodel, x, criterion)
+
+    attack, p = attack_and_p
+    advs = attack.run(fmodel, x, criterion, starting_points=init_advs)
+
+    init_norms = ep.norms.lp(flatten(init_advs - x), p=p, axis=-1)
+    norms = ep.norms.lp(flatten(advs - x), p=p, axis=-1)
+
+    is_smaller = norms < init_norms
+
+    assert fbn.accuracy(fmodel, advs, y) < fbn.accuracy(fmodel, x, y)
+    assert fbn.accuracy(fmodel, advs, target_classes) >= fbn.accuracy(
+        fmodel, init_advs, target_classes
+    )
+    assert is_smaller.any()
+
+
+def test_brendel_bethge_attack_no_init_no_starting_points(
+    fmodel_and_data_ext_for_attacks: Tuple[Tuple[fbn.Model, ep.Tensor, ep.Tensor], bool]
+) -> None:
+    (fmodel, x, y), real = fmodel_and_data_ext_for_attacks
+
+    if isinstance(x, ep.NumPyTensor):
+        pytest.skip()
+
+    x = (x - fmodel.bounds.lower) / (fmodel.bounds.upper - fmodel.bounds.lower)
+    fmodel = fmodel.transform_bounds((0, 1))
+
+    # run this test only on one of the BrendelBethge attacks as this
+    # only tests code that is used by all of the attacks
+    attack = fa.L2BrendelBethgeAttack()
+    advs = attack.run(fmodel, x, y)
+
+    assert fbn.accuracy(fmodel, advs, y) < fbn.accuracy(fmodel, x, y)
+
+
+def test_brendel_bethge_attack_no_starting_points(
+    fmodel_and_data_ext_for_attacks: Tuple[Tuple[fbn.Model, ep.Tensor, ep.Tensor], bool]
+) -> None:
+    (fmodel, x, y), real = fmodel_and_data_ext_for_attacks
+
+    if isinstance(x, ep.NumPyTensor):
+        pytest.skip()
+
+    x = (x - fmodel.bounds.lower) / (fmodel.bounds.upper - fmodel.bounds.lower)
+    fmodel = fmodel.transform_bounds((0, 1))
+
+    init_attack = fa.DatasetAttack()
+    init_attack.feed(fmodel, x)
+
+    # run this test only on one of the BrendelBethge attacks as this
+    # only tests code that is used by all of the attacks
+    attack = fa.L2BrendelBethgeAttack(init_attack=init_attack)
+    advs = attack.run(fmodel, x, y)
+
+    assert fbn.accuracy(fmodel, advs, y) < fbn.accuracy(fmodel, x, y)
