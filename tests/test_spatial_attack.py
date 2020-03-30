8f3136c227cdf5ce515dba94bd0f52df8f29c73e
lukas.schott <lukas.schott@gmail.com>
2020-03-30 21:47:21 +0200

    spatial_attack (#512)
    * init commit unfinished
    
    * rename and pep8
    
    * removed unused function
    
    * fixed translations, attack for grid and random
    
    * black formatting
    
    * removed debug prints
    
    * added tests and minor fixes
    
    * init commit unfinished
    
    * rename and pep8
    
    * removed unused function
    
    * fixed translations, attack for grid and random
    
    * black formatting
    
    * removed debug prints
    
    * imports only if requred
    
    * minor fixes
    
    * make black
    
    * added astensor
    
    * type annotations
    
    * black
    
    * fixed type errors
    
    * fixed test type annotations
    
    * fixed unused import
    
    * mypy test ignore due to rare case of inheritance
    
    * removed unused function
    
    * style fixes suggested by Roland
    
    * flake8
    
    * Removed unnecessary "----"
    
    * implemented repeated version
    
    * test repeated

---
+++ tests/test_spatial_attack.py
@@ -0,0 +1,40 @@
+from typing import List, Tuple
+import pytest
+import eagerpy as ep
+import foolbox as fbn
+import foolbox.attacks as fa
+
+
+def get_attack_id(x: fbn.Attack) -> str:
+    return repr(x)
+
+
+# attack
+attacks: List[Tuple[fbn.Attack, bool]] = [
+    (fa.SpatialAttack(), False),
+    (fa.SpatialAttack(grid_search=False), False),
+    (fa.SpatialAttack(grid_search=False), True),
+]
+
+
+@pytest.mark.parametrize("attack_grad_real", attacks, ids=get_attack_id)
+def test_spatial_attacks(
+    fmodel_and_data_ext_for_attacks: Tuple[
+        Tuple[fbn.Model, ep.Tensor, ep.Tensor], bool
+    ],
+    attack_grad_real: Tuple[fbn.Attack, bool],
+) -> None:
+
+    attack, repeated = attack_grad_real
+    if repeated:
+        attack = attack.repeat(2)
+    (fmodel, x, y), real = fmodel_and_data_ext_for_attacks
+    if not real:
+        pytest.skip()
+
+    x = (x - fmodel.bounds.lower) / (fmodel.bounds.upper - fmodel.bounds.lower)
+    fmodel = fmodel.transform_bounds((0, 1))
+    acc = fbn.accuracy(fmodel, x, y)
+    assert acc > 0
+    advs, _, _ = attack(fmodel, x, y)  # type: ignore
+    assert fbn.accuracy(fmodel, advs, y) < acc
