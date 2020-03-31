f535fadd1a33d9b393f4fa2cc424c16ba61614fb
lukas-schott <lukas.schott@gmail.com>
2020-03-31 12:36:36 +0200

    example run of spatial attack

---
+++ examples/spatial_attack.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import torchvision.models as models
+import eagerpy as ep
+from foolbox import PyTorchModel, accuracy, samples
+import foolbox.attacks as fa
+import numpy as np
+
+
+if __name__ == "__main__":
+    # instantiate a model
+    model = models.resnet18(pretrained=True).eval()
+    preprocessing = dict(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], axis=-3)
+    fmodel = PyTorchModel(model, bounds=(0, 1), preprocessing=preprocessing)
+
+    # get data and test the model
+    # wrapping the tensors with ep.astensors is optional, but it allows
+    # us to work with EagerPy tensors in the following
+    images, labels = ep.astensors(*samples(fmodel, dataset="imagenet", batchsize=16))
+    print("accuracy")
+    print(accuracy(fmodel, images, labels))
+    print("")
+
+    attack = fa.spatial_attack.SpatialAttack(
+        max_translation=6,  # 5px so in [x-5, x+5] and y in [y-5, y+5]
+        num_translations=6,   # number of translations in x, y. So 6*6*rot total
+        max_rotation=20,  # +- rotation in degrees
+        num_rotations=5,  # number of rotations, so 5*number_trans**2 total
+    )
+
+    xp_, _, success = attack(fmodel, images, labels)
+    print('attack success in specified rotation in translation bounds',
+          success.numpy().astype(np.float32).mean() * 100, ' %')
