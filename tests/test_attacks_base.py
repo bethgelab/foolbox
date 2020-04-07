94f71c6aa4b6763a663476fbd1bb73ac639ad38e
zimmerrol <rzrolandzimmermann@gmail.com>
2020-04-07 14:21:30 +0200

    Update unit tests

--- tests/test_attacks_base.py
+++ tests/test_attacks_base.py
@@ -45,21 +45,3 @@ def test_get_channel_axis() -> None:
     model.data_format = "invalid"  # type: ignore
     with pytest.raises(ValueError):
         assert fbn.attacks.base.get_channel_axis(model, 3)  # type: ignore
-
-
-def test_transform_bounds_wrapper_data_format() -> None:
-    class Model(fbn.models.Model):
-        data_format = "channels_first"
-
-        @property
-        def bounds(self) -> fbn.types.Bounds:
-            return fbn.types.Bounds(0, 1)
-
-        def __call__(self, inputs: fbn.models.base.T) -> fbn.models.base.T:
-            return inputs
-
-    model = Model()
-    wrapped_model = fbn.models.TransformBoundsWrapper(model, (0, 1))
-    assert fbn.attacks.base.get_channel_axis(
-        model, 3
-    ) == fbn.attacks.base.get_channel_axis(wrapped_model, 3)
