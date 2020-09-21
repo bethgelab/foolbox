bb559bcfac3aca366f73cba75dafc47084630017
zimmerrol <rzrolandzimmermann@gmail.com>
2020-09-21 11:30:13 +0200

    Remove targeted FGM/FGSM attacks

--- foolbox/attacks/fast_gradient_method.py
+++ foolbox/attacks/fast_gradient_method.py
@@ -1,6 +1,10 @@
 from .gradient_descent_base import L1BaseGradientDescent
 from .gradient_descent_base import L2BaseGradientDescent
 from .gradient_descent_base import LinfBaseGradientDescent
+from ..models.base import Model
+from ..criteria import Misclassification, TargetedMisclassification
+from .base import T
+from typing import Union, Any
 
 
 class L1FastGradientAttack(L1BaseGradientDescent):
@@ -15,6 +19,18 @@ class L1FastGradientAttack(L1BaseGradientDescent):
             rel_stepsize=1.0, steps=1, random_start=random_start,
         )
 
+    def run(
+        self,
+        model: Model,
+        inputs: T,
+        criterion: Union[Misclassification, T],
+        *,
+        epsilon: float,
+        **kwargs: Any,
+    ) -> T:
+        assert not isinstance(criterion, TargetedMisclassification)
+        super().run(model, inputs, criterion, epsilon, **kwargs)
+
 
 class L2FastGradientAttack(L2BaseGradientDescent):
     """Fast Gradient Method (FGM)
@@ -28,6 +44,18 @@ class L2FastGradientAttack(L2BaseGradientDescent):
             rel_stepsize=1.0, steps=1, random_start=random_start,
         )
 
+    def run(
+        self,
+        model: Model,
+        inputs: T,
+        criterion: Union[Misclassification, T],
+        *,
+        epsilon: float,
+        **kwargs: Any,
+    ) -> T:
+        assert not isinstance(criterion, TargetedMisclassification)
+        super().run(model, inputs, criterion, epsilon, **kwargs)
+
 
 class LinfFastGradientAttack(LinfBaseGradientDescent):
     """Fast Gradient Sign Method (FGSM)
@@ -40,3 +68,15 @@ class LinfFastGradientAttack(LinfBaseGradientDescent):
         super().__init__(
             rel_stepsize=1.0, steps=1, random_start=random_start,
         )
+
+    def run(
+        self,
+        model: Model,
+        inputs: T,
+        criterion: Union[Misclassification, T],
+        *,
+        epsilon: float,
+        **kwargs: Any,
+    ) -> T:
+        assert not isinstance(criterion, TargetedMisclassification)
+        super().run(model, inputs, criterion, epsilon, **kwargs)
--- tests/test_attacks.py
+++ tests/test_attacks.py
@@ -175,10 +175,6 @@ targeted_attacks: List[Tuple[fbn.Attack, Optional[float], bool, bool]] = [
     (fa.L2BasicIterativeAttack(), L2(50.0), True, False),
     (fa.L1BasicIterativeAttack(), 5000.0, True, False),
     (fa.SparseL1DescentAttack(), 5000.0, True, False),
-    (fa.FGSM(), Linf(100.0), True, False),
-    (FGSM_GE(), Linf(100.0), False, False),
-    (fa.FGM(), L2(100.0), True, False),
-    (fa.L1FastGradientAttack(), 5000.0, True, False),
     (fa.GenAttack(steps=100, population=6, reduced_dims=(7, 7)), 0.5, False, True),
 ]
 
