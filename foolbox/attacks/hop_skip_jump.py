eeeb18d494db3678f017d86848d2d18eef90b22a
zhuangzi926 <zhuangzi926@qq.com>
2021-01-22 16:28:04 +0800

    Fix HopSkipJump Attack gradient approximation bug

--- foolbox/attacks/hop_skip_jump.py
+++ foolbox/attacks/hop_skip_jump.py
@@ -251,7 +251,7 @@ class HopSkipJump(MinimizationAttack):
         perturbed = ep.expand_dims(x_advs, 0) + scaled_rv
         perturbed = ep.clip(perturbed, 0, 1)
 
-        rv = (perturbed - x_advs) / 2
+        rv = (perturbed - x_advs) / atleast_kd(ep.expand_dims(delta, 0), rv.ndim)
 
         multipliers_list: List[ep.Tensor] = []
         for step in range(steps):
