4afa17472768a4b71eb552cd0ce513bb6a3c0db1
zimmerrol <rzrolandzimmermann@gmail.com>
2022-02-04 18:23:02 +0100

    Undo change

--- tests/test_attacks.py
+++ tests/test_attacks.py
@@ -282,6 +282,6 @@ def test_targeted_attacks(
         )
         adv_after_attack = criterion(advs, fmodel(advs))
         adv_asr = adv_after_attack.sum().item()
-        if adv_asr > asr or (adv_asr == asr and asr == 1.0):
+        if adv_asr > asr:
             break
-    assert adv_asr > asr or (adv_asr == asr and asr == 1.0)
+    assert adv_asr > asr
