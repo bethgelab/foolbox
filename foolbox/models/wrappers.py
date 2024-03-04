55d2c8db674429d787dad49e88feddd20dbea0ec
Dario Serez <68145713+SerezD@users.noreply.github.com>
2024-03-04 10:18:24 +0100

    Expectation Over Transformation Wrapper (#719)
    * added Expectation Over Transformation Wrapper and example
    
    * added some tests for EoT Wrapper
    
    * compatibility with mypy, black, flake8
    
    * added docstring for EOT wrapper module

--- foolbox/models/wrappers.py
+++ foolbox/models/wrappers.py
@@ -21,3 +21,29 @@ class ThresholdingWrapper(Model):
         y = ep.where(x < self._threshold, min_, max_).astype(x.dtype)
         z = self._model(y)
         return restore_type(z)
+
+
+class ExpectationOverTransformationWrapper(Model):
+    def __init__(self, model: Model, n_steps: int = 16):
+        self._model = model
+        self._n_steps = n_steps
+
+    @property
+    def bounds(self) -> Bounds:
+        return self._model.bounds
+
+    def __call__(self, inputs: T) -> T:
+
+        x, restore_type = ep.astensor_(inputs)
+
+        for i in range(self._n_steps):
+            z_t = self._model(x)
+
+            if i == 0:
+                z = z_t.expand_dims(0)
+            else:
+                z = ep.concatenate([z, z_t.expand_dims(0)], axis=0)
+
+        z = z.mean(0)
+
+        return restore_type(z)
